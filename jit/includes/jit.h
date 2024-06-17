c95992cee6cb407ffe94a811f1fb723b019700c1
ashwin <ashwinbose6@gmail.com>
2024-06-17 11:22:22 +0100

    rewriting and separating jit module

---
+++ jit/includes/jit.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <vector>
+#include <cstdint>
+
+typedef instruction std::vector<uint8_t>;
+
+enum Register64 {
+    RAX, RBX, RCX, RDX, RSI, RDI, RBP, RSP,
+    R8, R9, R10, R11, R12, R13, R14, R15,
+};
+
+enum SIMDRegister {
+    XMM0,
+    XMM1,
+    XMM2,
+    XMM3,
+    XMM4,
+    XMM5,
+    XMM6,
+    XMM7
+};
